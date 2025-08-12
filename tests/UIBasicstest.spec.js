5a77479a29b73e0fd6c8c9f7846c7675d129504e
Celeena Alonzo <alonzoceleena@gmail.com>
2025-08-12 22:45:09 +0800

    First Playwright Testing - Learning the assertion toHaveTitle v2

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -6,6 +6,7 @@ test ('Browser Context Playwright test', async ({browser}) =>
         const context = await browser.newContext();
         const page = await context.newPage();
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        console.log(await page.title());
 
     }
 );
