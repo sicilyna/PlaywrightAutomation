af6a7f3a5a69333cc8d312685470eedbdf2dae4b
Celeena Alonzo <alonzoceleena@gmail.com>
2025-08-13 00:56:36 +0800

    First Playwright Automation - Add Page Locators and Validate Login Page v2

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -9,7 +9,7 @@ test('Browser Context Playwright test', async ({browser}) =>
         console.log(await page.title());
         //css, xpath selectors
         await page.locator('#username').fill("rahulshetty")
-        await page.locator("'[type=password]'").fill("learning");
+        await page.locator("[type='password']").fill("learning");
         await page.locator('#signInBtn').click();
     }
 );
