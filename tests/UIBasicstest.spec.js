6563125ee674c2618528876004b95d4e6fc72a3e
Celeena Alonzo <alonzoceleena@gmail.com>
2025-08-12 22:33:01 +0800

    First Playwright Automation - Vavavoom panes

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,18 @@
+const {test} = require('@playwright/test');
+
+test ('Browser Context Playwright test', async ({browser}) =>
+    {
+        //chrome - plugins / cookies
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    }
+);
+
+test.only('Page Playwright test', async ({page}) =>
+    {
+        await page.goto("https://google.com");
+
+    }
+);
